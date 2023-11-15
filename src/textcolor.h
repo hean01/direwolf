e77e5f9c0a8889c7c25cab6fde3d779fe5f024a1
Henrik Andersson <henrik.4e@gmail.com>
2023-11-15 22:17:31 +0100

    Cleanup of the messy vt100 code
    - First off the "themes" are removed because there are expectations
      how colors are used in terminal program. We should not paint as we
      like but respect the terminal theme selected by terminal / and user
      him self.
    
    - Use the correct way of setting up windows console for vt100 codes
      instead of using its specific colorpainting functions. This makes
      us to share the code on any platform as long as terminal supports
      vt100.

--- src/textcolor.h
+++ src/textcolor.h
@@ -10,18 +10,19 @@
 
 #ifndef TEXTCOLOR_H
 #define TEXTCOLOR_H 1
-
-enum dw_color_e { 	DW_COLOR_INFO,		/* black */
-			DW_COLOR_ERROR,		/* red */
-			DW_COLOR_REC,		/* green */
-			DW_COLOR_DECODED,	/* blue */
-			DW_COLOR_XMIT,		/* magenta */
-			DW_COLOR_DEBUG		/* dark_green */
-		};
+#include <stdarg.h>
+enum dw_color_e {
+    DW_COLOR_INFO = 0, 	/* foreground */
+    DW_COLOR_ERROR,	 	/* red */
+    DW_COLOR_REC,		/* green */
+    DW_COLOR_DECODED,	/* blue */
+    DW_COLOR_XMIT,		/* magenta */
+    DW_COLOR_DEBUG		/* dark_white */
+};
 
 typedef enum dw_color_e dw_color_t;
 
-			
+	
 void text_color_init (int enable_color);
 void text_color_set (dw_color_t c);
 void text_color_term (void);
@@ -48,11 +49,7 @@ void text_color_term (void);
 
 
 
-int dw_printf (const char *fmt, ...) 
-#if __WIN32__
-				__attribute__((format(ms_printf,1,2)));		/* Win C lib. */
-#else
-				__attribute__((format(printf,1,2)));		/* gnu C lib. */
-#endif
+int dw_printf(const char *fmt, ...);
+int dw_fprintf(FILE *stream, const char *fmt, ...);
 
 #endif
